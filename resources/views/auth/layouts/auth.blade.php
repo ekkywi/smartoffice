cca09f269d3712237ebab925481c5605f165091d
Yon Ekky Wijayanto <yonekkywijayanto@outlook.com>
2024-12-31 14:51:23 +0700

    login page added

---
+++ resources/views/auth/layouts/auth.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="ie=edge" http-equiv="X-UA-Compatible">
+    <title>@yield("title")</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+    <link href="{{ asset("css/auth/auth.css") }}" rel="stylesheet">
+</head>
+
+<body>
+
+    @yield("content")
+
+</body>
+
+</html>
---
+++ resources/views/auth/pages/login.blade.php
@@ -0,0 +1,62 @@
+@extends("auth.layouts.auth")
+@section("content")
+@section("title", "SmartManage - Login")
+<section class="h-100 gradient-form">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10">
+                <div class="card rounded-3 text-black">
+                    <div class="row g-0">
+                        <div class="col-lg-6">
+                            <div class="card-body p-md-5 mx-md-4">
+                                <div class="text-center mb-4">
+                                    <img alt="logo" class="img-fluid" src="{{ asset("images/logo1.png") }}" style="max-width: 100%; height: auto;">
+                                    <p><strong>E-Office Application</strong></p>
+                                </div>
+                                @if (session("success"))
+                                    <div class="alert alert-success">
+                                        {{ session("success") }}
+                                    </div>
+                                @endif
+
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                <form action="#" method="POST">
+                                    @csrf
+                                    <div class="form-outline mb-4" data-mdb-input-init>
+                                        <label class="form-label" for="username">Username</label>
+                                        <input class="form-control" id="username" name="username" placeholder="Masukan username Anda" required type="text" />
+                                    </div>
+
+                                    <div class="form-outline mb-4" data-mdb-input-init>
+                                        <label class="form-label" for="password">Password</label>
+                                        <input class="form-control" id="password" name="password" placeholder="Masukan password Anda" required type="password" />
+                                    </div>
+                                    <div class="text-center pt-1 mb-5 pb-1">
+                                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3 font-weight-bold" data-mdb-button-init data-mdb-ripple-init type="submit">Log in</button>
+                                    </div>
+                                    <p class="text-center">Belum punya akun? <a href="#">Daftar</a></p>
+                                    <p class="text-center">Lupa password? <a href="#">Klik disini</a></p>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                            <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                <h4 class="mb-4 text-center">Masuk dan mulai kelola aset anda</h4>
+                                <p class="small mb-0 text-justify">SmartManage adalah aplikasi berbasis Web yang difungsikan untuk mengelola aset. Anda dapat mengelola aset anda mulai dari menambah, mengubah, dan menghapus aset.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
